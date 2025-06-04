1dbd742517f29b90efa65c739353b37466cdd5e8
pyhilandjy <hilandjy@gmail.com>
2025-06-04 16:43:13 +0900

    feat: 카테고리 관련 SQL 쿼리 추가 - 메인 및 서브 카테고리 삽입, 조회, 업데이트, 삭제 쿼리 구현

---
+++ app/db/post_query.py
@@ -0,0 +1,63 @@
+from sqlalchemy import text
+
+POST_MAIN_CATEGORY = text(
+    """
+    insert into category (title, user_id)
+    values (:title, :user_id)
+    returning id
+    """
+)
+
+GET_CATEGORIY_BY_ID = text(
+    """
+    select c1.id as id, c1.title as title, 
+           coalesce(
+               json_agg(
+                   json_build_object('id', c2.id, 'title', c2.title)
+               ) filter (where c2.id is not null), 
+               '[]'
+           ) as sub_categories
+    from category c1
+    left join category c2 on c1.id = c2.parents_id
+    where c1.id = :id
+    group by c1.id, c1.title
+    """
+)
+
+POST_SUB_CATEGORY = text(
+    """
+    insert into category (title, parents_id, user_id)
+    values (:title, :parents_id, :user_id)
+    """
+)
+
+GET_CATEGORIES = text(
+    """
+    select c1.id as id, c1.title as title, 
+           coalesce(
+               json_agg(
+                   json_build_object('id', c2.id, 'title', c2.title)
+               ) filter (where c2.id is not null), 
+               '[]'
+           ) as sub_categories
+    from category c1
+    left join category c2 on c1.id = c2.parents_id
+    where c1.user_id = :user_id and c1.parents_id is null
+    group by c1.id, c1.title
+    """
+)
+
+UPDATE_MAIN_CATEGORY = text(
+    """
+    update category
+    set title = :title
+    where id = :id and user_id = :user_id
+    """
+)
+
+DELETE_MAIN_CATEGORY = text(
+    """
+    delete from category
+    where id = :id and user_id = :user_id
+    """
+)
