cfede2cbfd2dfe9f44e4519d395cb280e7d736f6
pyhilandjy <hilandjy@gmail.com>
2025-06-11 17:40:42 +0900

    feat: 사용자 게시글 목록 조회 API 위치 변경 및 정리

--- app/router/contents_list.py
+++ app/router/contents_list.py
@@ -9,15 +9,6 @@ from app.services.contents_list import (
 router = APIRouter()
 
 
-@router.get("/contents-list")
-async def get_contents_list(user_id: str = Depends(get_current_user)):
-    """
-    사용자 게시글 목록 조회 API
-    """
-    user_id = str(user_id)
-    return await get_users_contents_list(user_id)
-
-
 @router.get("/contents-list/{category_id}")
 async def get_category_contents_list(
     category_id: str, user_id: str = Depends(get_current_user)
@@ -27,3 +18,12 @@ async def get_category_contents_list(
     """
     user_id = str(user_id)
     return await get_users_category_contents_list(user_id, category_id)
+
+
+@router.get("/contents-list")
+async def get_contents_list(user_id: str = Depends(get_current_user)):
+    """
+    사용자 게시글 목록 조회 API
+    """
+    user_id = str(user_id)
+    return await get_users_contents_list(user_id)
